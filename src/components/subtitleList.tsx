8453a9c3a1bc8da51d8e87c913930b8b14c98023
kitagawakeita <k.syarin610@gmail.com>
2021-10-22 13:56:55 +0900

    日付の変更

--- src/components/subtitleList.tsx
+++ src/components/subtitleList.tsx
@@ -11,15 +11,8 @@ type Props = {
 };
 
 export const SubtitleList = (props: Props) => {
-
   return (
     <div className="">
-    {/* <div className="grid grid-cols-3 gap-2 m-4 sm:grid-cols-4 md:grid-cols-5 lg:grid-cols-6 xl:grid-cols-7 2xl:grid-cols-8"> */}
-      {/* <AddSubtitle
-        userData={props.userData}
-        uuid={props.uuid}
-        getItemList={props.getItemList}
-      /> */}
       {props.items.map((item) => {
         return (
           <div key={item.id}>
