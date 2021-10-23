6ba6563f9689fe75c6e4edbf2aae1fd279b97b2c
kitagawakeita <k.syarin610@gmail.com>
2021-10-23 17:36:52 +0900

    typeをまとめる

---
+++ src/interface/type.ts
@@ -0,0 +1,10 @@
+export type Data = {
+  id: number;
+  user_id: string;
+  category: string;
+  title: string;
+  user_name: string;
+  categories_list: string[];
+  image_url: string;
+  targetAmount: number;
+};
