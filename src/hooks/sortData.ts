17b41ce4a0559307fe8d5d0961caba0365c53310
kitagawakeita <k.syarin610@gmail.com>
2021-10-24 18:17:23 +0900

    sortDataのhooks

---
+++ src/hooks/sortData.ts
@@ -0,0 +1,22 @@
+import type {UserData} from 'src/interface/type';
+
+//アイテムの並び変え
+export const sortData = (data: UserData[]) => {
+  const arr = data.sort(
+    (a: { buyDate: string[] }, b: { buyDate: string[] }) => {
+      if (Number(a.buyDate[2]) === Number(b.buyDate[2])) {
+        if (Number(a.buyDate[3]) < Number(b.buyDate[3])) {
+          return 1;
+        } else {
+          return -1;
+        }
+      }
+      if (Number(a.buyDate[2]) < Number(b.buyDate[2])) {
+        return 1;
+      } else {
+        return -1;
+      }
+    }
+  );
+  return arr;
+};
