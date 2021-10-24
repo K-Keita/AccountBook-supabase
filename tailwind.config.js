65246a97a5e603e0ee02cd307780d938361135c2
kitagawakeita <k.syarin610@gmail.com>
2021-10-24 18:18:06 +0900

    headlessUIのインストール

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,15 @@
 module.exports = {
+  mode: "jit",
   purge: ["./src/**/*.{js,ts,jsx,tsx}"],
   darkMode: false, // or 'media' or 'class'
   theme: {
+    minHeight: {
+      lg: "90vh",
+    },
     extend: {},
   },
   variants: {
     extend: {},
   },
   plugins: [],
-}
+};
