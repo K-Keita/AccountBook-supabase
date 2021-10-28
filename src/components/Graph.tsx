4c2fc0b46948dcce7f99fe06f11fe68ab0027191
kitagawakeita <k.syarin610@gmail.com>
2021-10-28 14:18:17 +0900

    optionsの変更

--- src/components/Graph.tsx
+++ src/components/Graph.tsx
@@ -1,44 +1,101 @@
+import { useEffect } from "react";
 import { Bar } from "react-chartjs-2";
 
 type Props = {
   data: any;
-}
-
-// const data = {
-//   // x 軸のラベル
-//   labels: ["1 月", "2 月", "3 月", "4 月", "5 月", "6 月", "7 月"],
-//   datasets: [
-//     {
-//       label: "Dataset",
-//       // データの値
-//       data: [65, 59, 80, 81, 56, 55, 40],
-//       // グラフの背景色
-//       backgroundColor: [
-//         "rgba(255, 99, 132, 0.2)",
-//         "rgba(255, 159, 64, 0.2)",
-//         "rgba(255, 205, 86, 0.2)",
-//         "rgba(75, 192, 192, 0.2)",
-//         "rgba(54, 162, 235, 0.2)",
-//         "rgba(153, 102, 255, 0.2)",
-//         "rgba(201, 203, 207, 0.2)",
-//       ],
-//       // グラフの枠線の色
-//       borderColor: [
-//         "rgb(255, 99, 132)",
-//         "rgb(255, 159, 64)",
-//         "rgb(255, 205, 86)",
-//         "rgb(75, 192, 192)",
-//         "rgb(54, 162, 235)",
-//         "rgb(153, 102, 255)",
-//         "rgb(201, 203, 207)",
-//       ],
-//       // グラフの枠線の太さ
-//       borderWidth: 1,
-//     },
-//   ],
-// };
+};
 
 // レンダリング
 export const Graph = (props: Props): JSX.Element => {
-  return <Bar className="text-white" data={props.data} />;
-}
+  const options = {
+    responsive: true,
+    animation: {
+      duration: 2000,
+      // easing: "easeInBounce",
+    },
+    plugins: {
+      //タイトル関連
+      legend: {
+        labels: {
+          color: "#fff",
+          font: {
+            size: 14,
+          },
+        },
+      },
+    },
+    scales: {
+      //x軸関連
+      x: {
+        grid: {
+          drawBorder: true,
+          color: "#fff", //borderの色
+        },
+        ticks: {
+          color: "#fff", //テキストの色
+          font: {
+            size: 12,
+          },
+        },
+      },
+      //y軸関連
+      y: {
+        grid: {
+          drawBorder: true,
+          color: "#fff",
+        },
+        ticks: {
+          color: "#fff",
+          font: {
+            size: 12,
+          },
+          beginAtZero: true,
+        },
+      },
+    },
+  };
+
+  // const options = {
+  //   responsive: true,
+  //   // maintainAspectRatio: true,
+  //   color: "white",
+  //   legend: {
+  //     labels: {
+  //       fontColor: "orange",
+  //     },
+  //   },
+  //   title: {
+  //     display: true,
+  //     fontColor: "blue",
+  //     text: "Custom Chart Title",
+  //   },
+  //   animation: {
+  //     duration: 2000,
+  //     // easing: "easeInBounce",
+  //   },
+  //   scales: {
+  //     yAxes: {
+  //       grid: {
+  //         drawBorder: true,
+  //         color: "#fff",
+  //       },
+  //       // ticks: {
+  //       //   beginAtZero: false,
+  //       //   fontColor: "red",
+  //       // },
+  //     },
+  //     xAxes: {
+  //       grid: {
+  //         drawBorder: true,
+  //         color: "#fff",
+  //       },
+  //       // ticks: {
+  //       //   fontColor: "blue",
+  //       //   beginAtZero: false,
+  //       // },
+  //     },
+  //   },
+  // };
+
+  return <Bar data={props.data} options={options} />;
+};
