d87bee433a0ca1323ea8ab7e5028814ea3ce24ab
kitagawakeita <k.syarin610@gmail.com>
2021-10-29 19:21:28 +0900

    chart.tsxの作成

---
+++ src/pages/chart.tsx
@@ -0,0 +1,213 @@
+import { Auth } from "@supabase/ui";
+import Link from "next/link";
+import { useRouter } from "next/router";
+import type { VFC } from "react";
+import { useCallback, useEffect, useState } from "react";
+import { Graph } from "src/components/Graph";
+import { sortData } from "src/hooks/sortData";
+import type { Data as TitleType } from "src/interface/type";
+import type { UserData } from "src/interface/type";
+import { client } from "src/libs/supabase";
+
+const d = new Date();
+const y = d.getFullYear();
+const m = d.getMonth() + 1;
+// const day = d.getDate();
+
+//全てのアイテムの取得
+const getItems = async (userID: string, year: number, month: number) => {
+  let { data, error } = await client
+    .from("users")
+    .select("*")
+    .eq("userID", userID);
+
+  if (!error && data) {
+    const userData = data[0];
+    ({ data, error } = await client
+      .from("purchasedItem")
+      .select("*")
+      .contains("date", [`year:${year}`, `month:${month}`])
+      .eq("userID", userID));
+
+    const newData = data?.reduce((sum, element) => {
+      return sum + element.price;
+    }, 0);
+
+    if (!error && data) {
+      return { userData: userData, items: data, totalPrice: newData };
+    } else {
+      return { userData: userData, items: null, totalPrice: null };
+    }
+  }
+
+  return { userData: null, items: null, totalPrice: null };
+};
+
+const Title: VFC = () => {
+  const { user } = Auth.useUser();
+
+  const [items, setItems] = useState<UserData[]>([]);
+  const [userData, setUserData] = useState<TitleType>();
+  const [totalPrice, setTotalPrice] = useState<number>();
+  const [categories, setCategories] = useState<string[]>([]);
+
+  const [year, setYear] = useState<number>(y);
+  const [month, setMonth] = useState<number>(m);
+
+  const router = useRouter();
+
+  //IDと同じカテゴリーの商品を取得
+  const getItemList = useCallback(
+    async (year, month) => {
+      if (user) {
+        const { userData, items, totalPrice } = await getItems(
+          user.id.toString(),
+          year,
+          month
+        );
+        if (userData) {
+          setUserData(userData);
+          setCategories(userData.categoryList);
+        } else {
+          router.push("/login");
+        }
+        if (items) {
+          setItems(sortData(items));
+          setTotalPrice(totalPrice);
+        }
+      }
+    },
+    [router, user]
+  );
+
+  useEffect(() => {
+    getItemList(year, month);
+  }, [user, getItemList, router, month, year]);
+
+  //前の月へ
+  const prevMonth = useCallback(() => {
+    if (month === 1) {
+      setYear((year) => {
+        return year - 1;
+      });
+      setMonth(12);
+      return;
+    }
+    setMonth((month) => {
+      return month - 1;
+    });
+  }, [month]);
+
+  //次の月へ
+  const nextMonth = useCallback(() => {
+    if (month === m && year === y) {
+      return false;
+    } else if (month === 12) {
+      setYear((year) => {
+        return year - 1;
+      });
+      setMonth(1);
+      return;
+    }
+
+    setMonth((month) => {
+      return month + 1;
+    });
+  }, [month, year]);
+
+  //カテゴリーごとの合計金額
+  const priceArr = userData?.categoryList.map((category) => {
+    const arr = items.filter((value) => {
+      return value.categoryID === category;
+    });
+    const totalPrice = arr.reduce((sum, element) => {
+      return sum + element.price;
+    }, 0);
+    return totalPrice;
+  });
+
+  return user ? (
+    <div className="relative z-40 pt-7 pb-16 w-full min-h-lg text-white bg-gradient-to-b from-dark via-green-200 to-blue-500 rounded-t-3xl md:p-5 md:w-1/2">
+      <div className="flex justify-between pb-3">
+        <Link href="/" passHref>
+          <button className="px-4 text-2xl">-Title-</button>
+        </Link>
+        <div className="flex">
+          <Link href="/category" passHref>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              className="mx-2 w-8 h-8"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth={1.2}
+                d="M7 21a4 4 0 01-4-4V5a2 2 0 012-2h4a2 2 0 012 2v12a4 4 0 01-4 4zm0 0h12a2 2 0 002-2v-4a2 2 0 00-2-2h-2.343M11 7.343l1.657-1.657a2 2 0 012.828 0l2.829 2.829a2 2 0 010 2.828l-8.486 8.485M7 17h.01"
+              />
+            </svg>
+          </Link>
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            className="mx-4 w-8 h-8"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke="currentColor"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              strokeWidth={1.0}
+              d="M10.325 4.317c.426-1.756 2.924-1.756 3.35 0a1.724 1.724 0 002.573 1.066c1.543-.94 3.31.826 2.37 2.37a1.724 1.724 0 001.065 2.572c1.756.426 1.756 2.924 0 3.35a1.724 1.724 0 00-1.066 2.573c.94 1.543-.826 3.31-2.37 2.37a1.724 1.724 0 00-2.572 1.065c-.426 1.756-2.924 1.756-3.35 0a1.724 1.724 0 00-2.573-1.066c-1.543.94-3.31-.826-2.37-2.37a1.724 1.724 0 00-1.065-2.572c-1.756-.426-1.756-2.924 0-3.35a1.724 1.724 0 001.066-2.573c-.94-1.543.826-3.31 2.37-2.37.996.608 2.296.07 2.572-1.065z"
+            />
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              strokeWidth={1.2}
+              d="M15 12a3 3 0 11-6 0 3 3 0 016 0z"
+            />
+          </svg>
+        </div>
+      </div>
+      <h2 className="p-4 text-4xl font-bold">Chart</h2>
+      {userData ? (
+        <div className="p-2 my-2">
+          <Graph arr={priceArr} labels={userData.categoryList} />
+        </div>
+      ) : null}
+      <h2 className="p-4 text-4xl font-bold">By Category</h2>
+      <p className="p-4 text-lg text-right">
+        合計<span>({items.length})</span>:{""}
+        <span className="mx-3 text-xl font-bold">
+          ¥{totalPrice?.toLocaleString()}
+        </span>
+      </p>
+      {categories.map((category) => {
+        const arr = items.filter((value) => {
+          return value.categoryID === category;
+        });
+        const totalPrice = arr.reduce((sum, element) => {
+          return sum + element.price;
+        }, 0);
+        return (
+          <div
+            className="flex justify-between py-1 my-5 mx-auto w-10/12 border-b "
+            key={category}
+          >
+            <div className="text-xl">
+              {category}
+              <span className="mx-1 text-lg">({arr.length})</span>
+            </div>
+            <div>
+              <p className="text-lg">¥{totalPrice.toLocaleString()}</p>
+            </div>
+          </div>
+        );
+      })}
+    </div>
+  ) : null;
+};
+
+export default Title;
