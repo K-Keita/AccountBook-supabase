b3b7933af2b0aef61ab68a68ebc2625d1ee3397f
kitagawakeita <k.syarin610@gmail.com>
2021-10-29 18:27:24 +0900

    高さの変更

--- src/layouts/footer.tsx
+++ src/layouts/footer.tsx
@@ -1,6 +1,6 @@
 export const Footer = () => {
   return (
-    <footer className="px-4 h-32 text-gray-600 bg-gray-200">
+    <footer className="px-4 h-4 text-gray-600 bg-gray-200">
       <small className="block py-2 text-center">&copy; 2021 my-house-account</small>
     </footer>
   );
