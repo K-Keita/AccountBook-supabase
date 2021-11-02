6ada47386412ba35154398d078eff3f4b0b6b505
kitagawakeita <k.syarin610@gmail.com>
2021-11-02 13:01:05 +0900

    メインページの変更

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -183,12 +183,60 @@ const Home = () => {
   //1日の平均金額(現在)
   const nowAverage = totalPrice / d.getDate();
 
-  if (user) {
-    return (
-      <div className="pt-1 min-h-lg">
-        <div className="fixed p-5 mt-5 w-full h-lg">
-          <div className="flex justify-between">
-            <Link href="/category" passHref>
+  return user ? (
+    <div className="pt-1 min-h-lg text-white">
+      <div className="fixed p-5 mt-2 w-full h-lg">
+        <div className="flex">
+          <h2 className="text-3xl px-2 text-center">TITLE</h2>
+          <Link href="/setting" passHref>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              className="mx-2 ml-auto w-8 h-8"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth={1.2}
+                d="M10.325 4.317c.426-1.756 2.924-1.756 3.35 0a1.724 1.724 0 002.573 1.066c1.543-.94 3.31.826 2.37 2.37a1.724 1.724 0 001.065 2.572c1.756.426 1.756 2.924 0 3.35a1.724 1.724 0 00-1.066 2.573c.94 1.543-.826 3.31-2.37 2.37a1.724 1.724 0 00-2.572 1.065c-.426 1.756-2.924 1.756-3.35 0a1.724 1.724 0 00-2.573-1.066c-1.543.94-3.31-.826-2.37-2.37a1.724 1.724 0 00-1.065-2.572c-1.756-.426-1.756-2.924 0-3.35a1.724 1.724 0 001.066-2.573c-.94-1.543.826-3.31 2.37-2.37.996.608 2.296.07 2.572-1.065z"
+              />
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth={1.2}
+                d="M15 12a3 3 0 11-6 0 3 3 0 016 0z"
+              />
+            </svg>
+          </Link>
+        </div>
+        <h2 className="text-5xl text-center mt-32">TITLE</h2>
+        <div className="my-8 flex justify-around">
+          {totalPrice ? (
+            <div className="w-1/2 border-r py-1">
+              <h3 className="text-4xl tracking-wide text-center">
+                ¥ {totalPrice.toLocaleString()}
+              </h3>
+              <p className="text-center text-sm">
+                残り：¥
+                {userData
+                  ? (userData.targetAmount - totalPrice).toLocaleString()
+                  : null}
+              </p>
+            </div>
+          ) : null}
+          <div className="w-1/2">
+            <AddItem
+              userData={userData}
+              uuid={user.id}
+              getItemList={getItemList}
+            />
+          </div>
+        </div>
+        <div className="flex justify-around mt-12">
+          <Link href="/category" passHref>
+            <div>
               <svg
                 xmlns="http://www.w3.org/2000/svg"
                 className="mx-2 w-8 h-8"
@@ -203,8 +251,11 @@ const Home = () => {
                   d="M7 21a4 4 0 01-4-4V5a2 2 0 012-2h4a2 2 0 012 2v12a4 4 0 01-4 4zm0 0h12a2 2 0 002-2v-4a2 2 0 00-2-2h-2.343M11 7.343l1.657-1.657a2 2 0 012.828 0l2.829 2.829a2 2 0 010 2.828l-8.486 8.485M7 17h.01"
                 />
               </svg>
-            </Link>
-            <Link href="/chart" passHref>
+              <p className="text-xs text-center">Category</p>
+            </div>
+          </Link>
+          <Link href="/chart" passHref>
+            <div>
               <svg
                 xmlns="http://www.w3.org/2000/svg"
                 className="mx-2 w-8 h-8"
@@ -219,202 +270,160 @@ const Home = () => {
                   d="M9 19v-6a2 2 0 00-2-2H5a2 2 0 00-2 2v6a2 2 0 002 2h2a2 2 0 002-2zm0 0V9a2 2 0 012-2h2a2 2 0 012 2v10m-6 0a2 2 0 002 2h2a2 2 0 002-2m0 0V5a2 2 0 012-2h2a2 2 0 012 2v14a2 2 0 01-2 2h-2a2 2 0 01-2-2z"
                 />
               </svg>
-            </Link>
-            <Link href="/setting" passHref>
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                className="mx-2 w-8 h-8"
-                fill="none"
-                viewBox="0 0 24 24"
-                stroke="currentColor"
-              >
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  strokeWidth={1.0}
-                  d="M10.325 4.317c.426-1.756 2.924-1.756 3.35 0a1.724 1.724 0 002.573 1.066c1.543-.94 3.31.826 2.37 2.37a1.724 1.724 0 001.065 2.572c1.756.426 1.756 2.924 0 3.35a1.724 1.724 0 00-1.066 2.573c.94 1.543-.826 3.31-2.37 2.37a1.724 1.724 0 00-2.572 1.065c-.426 1.756-2.924 1.756-3.35 0a1.724 1.724 0 00-2.573-1.066c-1.543.94-3.31-.826-2.37-2.37a1.724 1.724 0 00-1.065-2.572c-1.756-.426-1.756-2.924 0-3.35a1.724 1.724 0 001.066-2.573c-.94-1.543.826-3.31 2.37-2.37.996.608 2.296.07 2.572-1.065z"
-                />
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  strokeWidth={1.2}
-                  d="M15 12a3 3 0 11-6 0 3 3 0 016 0z"
-                />
-              </svg>
-            </Link>
-          </div>
-          <h2 className="mt-24 text-5xl text-center">TITLE</h2>
-          <div className="mt-16 mb-8">
-            <AddItem
-              userData={userData}
-              uuid={user.id}
-              getItemList={getItemList}
-            />
-          </div>
-          {totalPrice ? (
-            <>
-              <h3 className="px-8 pt-2 text-4xl tracking-wide text-center ">
-                ¥ {totalPrice.toLocaleString()}
-              </h3>
-              <p className="text-center">
-                残り：¥
-                {userData
-                  ? (userData.targetAmount - totalPrice).toLocaleString()
-                  : null}
-              </p>
-            </>
-          ) : null}
+              <p className="text-xs text-center">Chart</p>
+            </div>
+          </Link>
         </div>
-        <div className="relative -z-10 h-lg opacity-0" />
-        <div className="relative z-40 pt-10 w-full h-screen bg-gradient-to-b from-dark via-green-200 to-blue-500 rounded-t-3xl md:p-5 md:w-1/2">
-          <div className="flex px-4">
-            <button onClick={prevMonth}>
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                className="w-6 h-6"
-                fill="none"
-                viewBox="0 0 24 24"
-                stroke="currentColor"
-              >
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  strokeWidth={2}
-                  d="M11 19l-7-7 7-7m8 14l-7-7 7-7"
-                />
-              </svg>
-            </button>
-            <h2 className="p-2 text-2xl">{month}月</h2>
-            <button onClick={nextMonth}>
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                className="w-6 h-6"
-                fill="none"
-                viewBox="0 0 24 24"
-                stroke="currentColor"
-              >
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  strokeWidth={2}
-                  d="M13 5l7 7-7 7M5 5l7 7-7 7"
-                />
-              </svg>
-            </button>
-            <div className="mx-4 ml-auto text-sm border-white">
-              <p>
-                使用金額(月)：
-                <span className="text-base">
-                  ¥{userData?.targetAmount.toLocaleString()}
-                </span>
-              </p>
-              <p className="text-center">
-                (平均金額：
-                {targetAverage
-                  ? Math.floor(targetAverage).toLocaleString()
-                  : null}
-                )
-              </p>
-            </div>
+      </div>
+      <div className="relative -z-10 h-lg opacity-0" />
+      <div className="relative z-40 pt-10 w-full h-screen bg-gradient-to-b from-dark via-green-200 to-blue-500 rounded-t-3xl md:p-5 md:w-1/2">
+        <div className="flex px-4">
+          <button onClick={prevMonth}>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              className="w-6 h-6"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth={2}
+                d="M11 19l-7-7 7-7m8 14l-7-7 7-7"
+              />
+            </svg>
+          </button>
+          <h2 className="p-2 text-2xl">{month}月</h2>
+          <button onClick={nextMonth}>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              className="w-6 h-6"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth={2}
+                d="M13 5l7 7-7 7M5 5l7 7-7 7"
+              />
+            </svg>
+          </button>
+          <div className="mx-4 ml-auto text-sm border-white">
+            <p>
+              使用金額(月)：
+              <span className="text-base">
+                ¥{userData?.targetAmount.toLocaleString()}
+              </span>
+            </p>
+            <p className="text-center">
+              (平均金額：
+              {targetAverage
+                ? Math.floor(targetAverage).toLocaleString()
+                : null}
+              )
+            </p>
           </div>
-          <Tab.Group defaultIndex={day - 1}>
-            <Tab.List
-              id="sc"
-              className="flex overflow-x-scroll flex-nowrap py-3 px-4 mx-auto mt-3 space-x-1 w-11/12 border-b"
-            >
-              {thisMonthDays.map((category) => {
-                return (
-                  <Tab
-                    key={category}
-                    disabled={category > day}
-                    className={({ selected }) => {
-                      return classNames(
-                        `min-w-lg py-2.5 text-lg font-semibold leading-5 rounded-lg ${
-                          category > day ? "text-gray-400" : "text-blue-600"
-                        }`,
-                        "focus:outline-none focus:ring-1 ring-opacity-60",
-                        selected
-                          ? "shadow bg-selected bg-opacity-50"
-                          : `${
-                              category > day
-                                ? ""
-                                : "text-blue-100 hover:bg-white/[0.12] hover:text-white"
-                            }`
-                      );
-                    }}
-                  >
-                    {category}
-                  </Tab>
-                );
-              })}
-            </Tab.List>
-            <div
-              className={`${
-                isTop ? "block" : "hidden"
-              } animate-slide-in-bck-center ml-auto mt-5 w-1/2`}
-            >
-              <p className="py-1">
-                今日の金額：
-                {oneDayTotalPrice.toLocaleString()}円
-              </p>
-              <p className="text-sm">
-                1日の平均金額：
-                {nowAverage ? Math.floor(nowAverage).toLocaleString() : null}円
-              </p>
-            </div>
+        </div>
+        <Tab.Group defaultIndex={day - 1}>
+          <Tab.List
+            id="sc"
+            className="flex overflow-x-scroll flex-nowrap py-3 px-4 mx-auto mt-3 space-x-1 w-11/12 border-b"
+          >
             {thisMonthDays.map((category) => {
-              const item = items.filter((value) => {
-                return value.buyDate[2] === category.toString();
-              });
-              const totalItems = item.reduce((sum, element) => {
-                return sum + element.price;
-              }, 0);
               return (
-                <Tab.Panels key={category}>
-                  <Tab.Panel
-                    className={classNames(
-                      "rounded-b-xl",
-                      "focus:outline-none focus:ring-2 ring-offset-2 ring-offset-blue-400 ring-white ring-opacity-60"
-                    )}
+                <Tab
+                  key={category}
+                  disabled={category > day}
+                  className={({ selected }) => {
+                    return classNames(
+                      `min-w-lg py-2.5 text-lg font-semibold leading-5 rounded-lg ${
+                        category > day ? "text-gray-400" : "text-blue-600"
+                      }`,
+                      "focus:outline-none focus:ring-1 ring-opacity-60",
+                      selected
+                        ? "shadow bg-selected bg-opacity-50"
+                        : `${
+                            category > day
+                              ? ""
+                              : "text-blue-100 hover:bg-white/[0.12] hover:text-white"
+                          }`
+                    );
+                  }}
+                >
+                  {category}
+                </Tab>
+              );
+            })}
+          </Tab.List>
+          <div
+            className={`${
+              isTop ? "block" : "hidden"
+            } animate-slide-in-bck-center ml-auto mt-5 w-1/2`}
+          >
+            <p className="py-1">
+              今日の金額：
+              {oneDayTotalPrice.toLocaleString()}円
+            </p>
+            <p className="text-sm">
+              1日の平均金額：
+              {nowAverage ? Math.floor(nowAverage).toLocaleString() : null}円
+            </p>
+          </div>
+          {thisMonthDays.map((category) => {
+            const item = items.filter((value) => {
+              return value.buyDate[2] === category.toString();
+            });
+            const totalItems = item.reduce((sum, element) => {
+              return sum + element.price;
+            }, 0);
+            return (
+              <Tab.Panels key={category}>
+                <Tab.Panel
+                  className={classNames(
+                    "rounded-b-xl",
+                    "focus:outline-none focus:ring-2 ring-offset-2 ring-offset-blue-400 ring-white ring-opacity-60"
+                  )}
+                >
+                  <div
+                    className={`text-xl py-3 mx-4 px-4 font-semibold ${
+                      isTop ? "hidden" : "block animate-slide-in-bck-center"
+                    }`}
                   >
-                    <div
-                      className={`text-xl py-3 mx-4 px-4 font-semibold ${
-                        isTop ? "hidden" : "block animate-slide-in-bck-center"
-                      }`}
-                    >
-                      total: ¥
+                    total: ¥
+                    {category.toString() === "全て"
+                      ? totalPrice.toLocaleString()
+                      : totalItems.toLocaleString()}
+                  </div>
+                  <div
+                    className={`${
+                      isTop ? "block" : "hidden"
+                    } animate-slit-in-vertical text-base mx-4 py-3 my-3 px-4 table border-r`}
+                  >
+                    total:
+                    <span className="block text-3xl font-bold">
+                      ¥
                       {category.toString() === "全て"
                         ? totalPrice.toLocaleString()
                         : totalItems.toLocaleString()}
-                    </div>
-                    <div
-                      className={`${
-                        isTop ? "block" : "hidden"
-                      } animate-slit-in-vertical text-base mx-4 py-3 my-3 px-4 table border-r`}
-                    >
-                      total:
-                      <span className="block text-3xl font-bold">
-                        ¥
-                        {category.toString() === "全て"
-                          ? totalPrice.toLocaleString()
-                          : totalItems.toLocaleString()}
-                      </span>
-                    </div>
-                    <ItemList
-                      items={category.toString() === "全て" ? items : item}
-                      userData={userData}
-                      uuid={user.id}
-                      getItemList={getItemList}
-                    />
-                  </Tab.Panel>
-                </Tab.Panels>
-              );
-            })}
-          </Tab.Group>
-        </div>
+                    </span>
+                  </div>
+                  <ItemList
+                    items={category.toString() === "全て" ? items : item}
+                    userData={userData}
+                    uuid={user.id}
+                    getItemList={getItemList}
+                  />
+                </Tab.Panel>
+              </Tab.Panels>
+            );
+          })}
+        </Tab.Group>
       </div>
-    );
-  }
+    </div>
+  ) : null;
 };
 
 export default Home;
