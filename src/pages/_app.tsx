fa4da95f6830f039b0ff022e6a0c421422fcfb12
kitagawakeita <k.syarin610@gmail.com>
2021-11-02 10:27:21 +0900

    login認証で分岐

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,55 +1,41 @@
+/* eslint-disable react-hooks/exhaustive-deps */
 import "tailwindcss/tailwind.css";
 
-// import { useState, useEffect } from "react";
-// import { Auth } from "@supabase/ui";
 import type { AppProps } from "next/app";
+import { useRouter } from "next/router";
 import PropTypes from "prop-types";
+import { useEffect } from "react";
 import { LayoutWrapper } from "src/layouts/layoutWrapper";
 import { client } from "src/libs/supabase";
 
 const MyApp = (props: AppProps): JSX.Element => {
   const user = client.auth.user();
-  // const [session, setSession] = useState<any>();
-  // useEffect(() => {
-  //   client.auth.onAuthStateChange(
-  //     (event, session) => {
-  //       setSession(session);
-  //     }
-  //   );
+  const router = useRouter();
 
-  //   return () => {
-  //     authListener.unsubscribe();
-  //   };
-  // }, []);
+  useEffect(() => {
+    if (!user) {
+      router.push("/");
+    }
+  }, [user]);
+
   const signInWithGoogle = async () => {
     const { error } = await client.auth.signIn({
       provider: "google",
     });
 
-    // console.log(user, session);
     if (error) {
       throw new Error("");
     }
   };
 
-  // console.log(session);
-  if (user) {
-    return (
-      <LayoutWrapper>
-        <props.Component {...props.pageProps} />
-      </LayoutWrapper>
-    );
-  }
-
   return (
-    <div className="w-full sm:w-96">
-      {/* <Auth
-            supabaseClient={client}
-            providers={["google"]}
-            // socialColors={true}
-          /> */}
-      <button onClick={signInWithGoogle}>signIn</button>
-    </div>
+    <LayoutWrapper>
+      {user ? (
+        <props.Component {...props.pageProps} />
+      ) : (
+        <button onClick={signInWithGoogle}>signIn</button>
+      )}
+    </LayoutWrapper>
   );
 };
 
