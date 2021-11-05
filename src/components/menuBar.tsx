73598d3b40772e43e18add511b93378aa9b801b7
kitagawakeita <k.syarin610@gmail.com>
2021-11-05 13:00:44 +0900

    menuBarの作成

---
+++ src/components/menuBar.tsx
@@ -0,0 +1,102 @@
+import Link from 'next/link';
+
+export const MenuBar = () => {
+
+  return (
+    <div className="flex fixed bottom-0 z-50 justify-around py-2 w-full bg-gradient-to-r from-blue-gray-900 via-purple-900 to-pink-900 rounded-t-xl">
+      {/* <AddItem
+            userData={userData}
+            uuid={user.id}
+            getItemList={getItemList}
+          /> */}
+      <Link href="/" passHref>
+        <div className="cursor-pointer">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            className="mx-4 w-8 h-8"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke="currentColor"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              strokeWidth={1}
+              d="M9 12l2 2 4-4M7.835 4.697a3.42 3.42 0 001.946-.806 3.42 3.42 0 014.438 0 3.42 3.42 0 001.946.806 3.42 3.42 0 013.138 3.138 3.42 3.42 0 00.806 1.946 3.42 3.42 0 010 4.438 3.42 3.42 0 00-.806 1.946 3.42 3.42 0 01-3.138 3.138 3.42 3.42 0 00-1.946.806 3.42 3.42 0 01-4.438 0 3.42 3.42 0 00-1.946-.806 3.42 3.42 0 01-3.138-3.138 3.42 3.42 0 00-.806-1.946 3.42 3.42 0 010-4.438 3.42 3.42 0 00.806-1.946 3.42 3.42 0 013.138-3.138z"
+            />
+          </svg>
+          <p className="text-xs text-center">registration</p>
+        </div>
+      </Link>
+      <Link href="/category" passHref>
+        <div className={`cursor-pointer`}>
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            className="mx-4 w-8 h-8"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke="currentColor"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              strokeWidth={1}
+              d="M7 21a4 4 0 01-4-4V5a2 2 0 012-2h4a2 2 0 012 2v12a4 4 0 01-4 4zm0 0h12a2 2 0 002-2v-4a2 2 0 00-2-2h-2.343M11 7.343l1.657-1.657a2 2 0 012.828 0l2.829 2.829a2 2 0 010 2.828l-8.486 8.485M7 17h.01"
+            />
+          </svg>
+          <p className="text-xs text-center">Category</p>
+        </div>
+      </Link>
+      <Link href="/chart" passHref>
+        <div className={`cursor-pointer`}>
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            className="mx-4 w-8 h-8"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke="currentColor"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              strokeWidth={1.2}
+              d="M11 3.055A9.001 9.001 0 1020.945 13H11V3.055z"
+            />
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              strokeWidth={1.3}
+              d="M20.488 9H15V3.512A9.025 9.025 0 0120.488 9z"
+            />
+          </svg>
+          <p className="text-xs text-center">Chart</p>
+        </div>
+      </Link>
+      <Link href="/setting" passHref>
+        <div className={`cursor-pointer`}>
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            className="mx-4 w-8 h-8"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke="currentColor"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              strokeWidth={1.2}
+              d="M10.325 4.317c.426-1.756 2.924-1.756 3.35 0a1.724 1.724 0 002.573 1.066c1.543-.94 3.31.826 2.37 2.37a1.724 1.724 0 001.065 2.572c1.756.426 1.756 2.924 0 3.35a1.724 1.724 0 00-1.066 2.573c.94 1.543-.826 3.31-2.37 2.37a1.724 1.724 0 00-2.572 1.065c-.426 1.756-2.924 1.756-3.35 0a1.724 1.724 0 00-2.573-1.066c-1.543.94-3.31-.826-2.37-2.37a1.724 1.724 0 00-1.065-2.572c-1.756-.426-1.756-2.924 0-3.35a1.724 1.724 0 001.066-2.573c-.94-1.543.826-3.31 2.37-2.37.996.608 2.296.07 2.572-1.065z"
+            />
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              strokeWidth={1.2}
+              d="M15 12a3 3 0 11-6 0 3 3 0 016 0z"
+            />
+          </svg>
+          <p className="text-xs text-center">Setting</p>
+        </div>
+      </Link>
+    </div>
+  );
+}
