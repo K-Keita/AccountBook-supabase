78a4c6917e66487d0e746ea39a7604e321d328b6
kitagawakeita <k.syarin610@gmail.com>
2021-11-05 15:47:16 +0900

    create ChangeButton

---
+++ src/components/utils/changeMonthButton.tsx
@@ -0,0 +1,45 @@
+type Props = {
+  prevMonth: () => void;
+  nextMonth: () => void;
+  month: number;
+};
+
+export const ChangeMonthButton = (props: Props) => {
+  return (
+    <>
+      <button onClick={props.prevMonth}>
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          className="w-6 h-6"
+          fill="none"
+          viewBox="0 0 24 24"
+          stroke="currentColor"
+        >
+          <path
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            strokeWidth={2}
+            d="M11 19l-7-7 7-7m8 14l-7-7 7-7"
+          />
+        </svg>
+      </button>
+      <h2 className="p-2 text-2xl">{props.month}月</h2>
+      <button onClick={props.nextMonth}>
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          className="w-6 h-6"
+          fill="none"
+          viewBox="0 0 24 24"
+          stroke="currentColor"
+        >
+          <path
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            strokeWidth={2}
+            d="M13 5l7 7-7 7M5 5l7 7-7 7"
+          />
+        </svg>
+      </button>
+    </>
+  );
+};
