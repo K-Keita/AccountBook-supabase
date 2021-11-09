f21993448c3062ab1d3026d59d05554329deeb7e
kitagawakeita <k.syarin610@gmail.com>
2021-11-09 15:13:13 +0900

    ✨ 認証部分のフォームをreact-hooks-formで実装

--- src/pages/logIn.tsx
+++ src/pages/logIn.tsx
@@ -1,15 +1,31 @@
 /* eslint-disable react-hooks/exhaustive-deps */
 import "tailwindcss/tailwind.css";
 
-import Image from "next/image";
-import Link from 'next/link';
+import Link from "next/link";
 import { useRouter } from "next/router";
 import { useEffect } from "react";
+import type { SubmitHandler } from "react-hook-form";
+import { useForm } from "react-hook-form";
+import { AuthButton } from "src/components/utils/authButton";
+import { GoogleButton } from "src/components/utils/googleButton";
+import { Title } from "src/components/utils/title";
+import { AuthLayout } from "src/layouts/authLayout";
 import { client } from "src/libs/supabase";
+
+type FormValues = {
+  email: string;
+  password: string;
+};
 
 const LogIn = () => {
   const user = client.auth.user();
   const router = useRouter();
+
+  const { register, handleSubmit } = useForm<FormValues>();
+
+  const onSubmit: SubmitHandler<FormValues> = (data) => {
+    signInWithEmail(data.email, data.password);
+  };
 
   useEffect(() => {
     if (user) {
@@ -43,57 +59,61 @@ const LogIn = () => {
   };
 
   return (
-    <>
-      <div
-        className="h-screen text-white bg-gradient-to-b from-blue-800 via-purple-900 to-danger"
-        style={{ fontFamily: "游明朝体" }}
-      >
-        <div className="absolute top-0 w-full bg-blue-600 bg-opacity-20">
-          <p className="py-3 text-sm text-center cursor-pointer">
-            <span className="opacity-80">アカウントをお持ちでない方 </span>
-            <Link href="/signUp" passHref><span className="hover:underline">登録はこちら</span></Link>
-          </p>
-        </div>
-        <div className="px-8 pt-28 pb-5">
-          <div className="flex relative justify-around border-b">
-            <h2 className="absolute top-20 z-40 text-3xl font-bold tracking-lg text-center animate-slit-in-vertical">
-              Title
-            </h2>
-            <div className="mt-auto w-8 h-24 bg-blue-400 animate-scale-in-ver-bottom"></div>
-            <div className="mt-auto w-8 h-12 bg-blue-400 animate-scale-in-ver-bottom"></div>
-            <div className="mt-auto w-8 h-20 bg-blue-400 animate-scale-in-ver-bottom"></div>
-            <div className="mt-auto w-8 h-32 bg-blue-400 animate-scale-in-ver-bottom"></div>
-            <div className="mt-auto w-8 h-16 bg-blue-400 animate-scale-in-ver-bottom"></div>
-          </div>
-        </div>
+    <AuthLayout>
+      <div className="absolute top-0 w-full bg-blue-600 bg-opacity-20">
+        <p className="py-3 text-sm text-center cursor-pointer">
+          <span className="opacity-80">アカウントをお持ちでない方 </span>
+          <Link href="/signUp" passHref>
+            <span className="hover:underline">登録はこちら</span>
+          </Link>
+        </p>
+      </div>
+      <div className="px-8 pt-28 pb-5">
+        <Title />
+      </div>
+      <form onSubmit={handleSubmit(onSubmit)}>
         <input
+          autoFocus
           placeholder="メールアドレス"
+          {...register("email")}
           className="block py-2 px-1 my-5 mx-auto w-9/12 bg-white bg-opacity-10"
         />
         <input
           placeholder="パスワード"
+          {...register("password")}
           className="block py-2 px-1 my-5 mx-auto w-9/12 bg-white bg-opacity-10"
         />
-        <button
+        <AuthButton text="ログイン" onClick={handleSubmit(onSubmit)} />
+      </form>
+      {/* <input
+        placeholder="メールアドレス"
+        className="block py-2 px-1 my-5 mx-auto w-9/12 bg-white bg-opacity-10"
+      />
+      <input
+        placeholder="パスワード"
+        className="block py-2 px-1 my-5 mx-auto w-9/12 bg-white bg-opacity-10"
+      /> */}
+      {/* <button
           onClick={() => {
             signInWithEmail("k.syarin2610@gmail.com", "12345678");
           }}
           className="block py-2 my-5 mx-auto w-9/12 text-center text-gray-600 bg-white bg-opacity-80 rounded-sm"
         >
           ログイン
-        </button>
-        <p className="text-xs text-center text-gray-100">
-          ログインパスワードを忘れてしまった方は
-          <span className="text-blue-500 hover:underline cursor-pointer">
-            こちら
-          </span>
-        </p>
-        <div className="flex mt-16 opacity-80">
-          <div className="mx-auto mb-3 w-3/12 border-b" />
-          <p className="">または</p>
-          <div className="mx-auto mb-3 w-3/12 border-b" />
-        </div>
-        <button
+        </button> */}
+      <p className="text-xs text-center text-gray-100">
+        ログインパスワードを忘れてしまった方は
+        <span className="text-blue-500 hover:underline cursor-pointer">
+          こちら
+        </span>
+      </p>
+      <div className="flex mt-16 opacity-80">
+        <div className="mx-auto mb-3 w-3/12 border-b" />
+        <p className="">または</p>
+        <div className="mx-auto mb-3 w-3/12 border-b" />
+      </div>
+      <GoogleButton text="Googleでログイン" onClick={signInWithGoogle} />
+      {/* <button
           onClick={signInWithGoogle}
           className="flex py-1 px-1 my-6 mx-auto w-9/12 text-center bg-google bg-opacity-50 rounded-sm"
         >
@@ -106,12 +126,8 @@ const LogIn = () => {
             />
           </div>
           <p className="flex-1 mt-1 text-center">Googleでログイン</p>
-        </button>
-        <div className="table absolute bottom-1 mx-auto w-full text-xs text-center">
-          &copy;My-house-account
-        </div>
-      </div>
-    </>
+        </button> */}
+    </AuthLayout>
   );
 };
 
--- src/pages/signUp.tsx
+++ src/pages/signUp.tsx
@@ -1,13 +1,29 @@
 /* eslint-disable react-hooks/exhaustive-deps */
 import "tailwindcss/tailwind.css";
 
-import Image from "next/image";
 import Link from "next/link";
 import { useRouter } from "next/router";
+import type { SubmitHandler } from "react-hook-form";
+import { useForm } from "react-hook-form";
+import { AuthButton } from "src/components/utils/authButton";
+import { GoogleButton } from "src/components/utils/googleButton";
+import { Title } from "src/components/utils/title";
+import { AuthLayout } from "src/layouts/authLayout";
 import { client } from "src/libs/supabase";
+
+type FormValues = {
+  email: string;
+  password: string;
+};
 
 const SignUp = () => {
   const router = useRouter();
+
+  const { register, handleSubmit } = useForm<FormValues>();
+
+  const onSubmit: SubmitHandler<FormValues> = (data) => {
+    signUpWithEmail(data.email, data.password);
+  };
 
   const signUpWithEmail = async (email: string, password: string) => {
     const { user, error } = await client.auth.signUp({
@@ -47,73 +63,63 @@ const SignUp = () => {
   };
 
   return (
-    <>
-      <div
-        className="h-screen text-white bg-gradient-to-b from-blue-800 via-purple-900 to-danger"
-        style={{ fontFamily: "游明朝体" }}
+    <AuthLayout>
+      <div className="absolute top-0 w-full bg-blue-600 bg-opacity-20">
+        <p className="py-3 text-sm text-center cursor-pointer">
+          <span className="opacity-80">既にアカウントをお持ちの方 </span>
+          <Link href="/logIn" passHref>
+            <span className="hover:underline">ログイン</span>
+          </Link>
+        </p>
+      </div>
+      <div className="px-8 pt-28 pb-5">
+        <Title />
+      </div>
+      <GoogleButton text="Googleで登録" onClick={signUpWithGoogle} />
+      {/* <button
+        onClick={() => {
+          signUpWithGoogle();
+        }}
+        className="flex py-1 px-1 mx-auto mt-24 w-9/12 text-center bg-google bg-opacity-50 rounded-sm"
       >
-        <div className="absolute top-0 w-full bg-blue-600 bg-opacity-20">
-          <p className="py-3 text-sm text-center cursor-pointer">
-            <span className="opacity-80">既にアカウントをお持ちの方 </span>
-            <Link href="/logIn" passHref>
-              <span className="hover:underline">ログイン</span>
-            </Link>
-          </p>
+        <div className="p-1 h-8 bg-white rounded-sm">
+          <Image
+            alt="Google_icon"
+            src="/icons/Google.png"
+            width={24}
+            height={24}
+          />
         </div>
-        <div className="px-8 pt-28 pb-5">
-          <div className="flex relative justify-around border-b">
-            <h2 className="absolute top-20 z-40 text-3xl font-bold tracking-lg text-center animate-slit-in-vertical">
-              Title
-            </h2>
-            <div className="mt-auto w-8 h-24 bg-blue-400 animate-scale-in-ver-bottom"></div>
-            <div className="mt-auto w-8 h-12 bg-blue-400 animate-scale-in-ver-bottom"></div>
-            <div className="mt-auto w-8 h-20 bg-blue-400 animate-scale-in-ver-bottom"></div>
-            <div className="mt-auto w-8 h-32 bg-blue-400 animate-scale-in-ver-bottom"></div>
-            <div className="mt-auto w-8 h-16 bg-blue-400 animate-scale-in-ver-bottom"></div>
-          </div>
-        </div>
-        <button
-          onClick={() => {
-            signUpWithGoogle();
-          }}
-          className="flex py-1 px-1 mx-auto mt-24 w-9/12 text-center bg-google bg-opacity-50 rounded-sm"
-        >
-          <div className="p-1 h-8 bg-white rounded-sm">
-            <Image
-              alt="Google_icon"
-              src="/icons/Google.png"
-              width={24}
-              height={24}
-            />
-          </div>
-          <p className="flex-1 mt-1 text-center">Googleで登録</p>
-        </button>
-        <div className="flex mt-8 opacity-80">
-          <div className="mx-auto mb-3 w-3/12 border-b" />
-          <p className="">または</p>
-          <div className="mx-auto mb-3 w-3/12 border-b" />
-        </div>
+        <p className="flex-1 mt-1 text-center">Googleで登録</p>
+      </button> */}
+      <div className="flex mt-8 opacity-80">
+        <div className="mx-auto mb-3 w-3/12 border-b" />
+        <p>または</p>
+        <div className="mx-auto mb-3 w-3/12 border-b" />
+      </div>
+      <form onSubmit={handleSubmit(onSubmit)}>
         <input
+          autoFocus
           placeholder="メールアドレス"
+          {...register("email")}
           className="block py-2 px-1 my-5 mx-auto w-9/12 bg-white bg-opacity-10"
         />
         <input
           placeholder="パスワード"
+          {...register("password")}
           className="block py-2 px-1 my-5 mx-auto w-9/12 bg-white bg-opacity-10"
         />
-        <button
-          onClick={() => {
-            signUpWithEmail("k.syarin2610@gmail.com", "12345678");
-          }}
-          className="block py-2 my-5 mx-auto w-9/12 text-center text-gray-600 bg-white bg-opacity-80 rounded-sm"
-        >
-          登録
-        </button>
-        <div className="table absolute bottom-1 py-1 mx-auto w-full text-xs text-center">
-          &copy;My-house-account
-        </div>
-      </div>
-    </>
+        <AuthButton text="登録" onClick={handleSubmit(onSubmit)} />
+      </form>
+      {/* <button
+        onClick={() => {
+          signUpWithEmail("k.syarin2610@gmail.com", "12345678");
+        }}
+        className="block py-2 my-5 mx-auto w-9/12 text-center text-gray-600 bg-white bg-opacity-80 rounded-sm"
+      >
+        登録
+      </button> */}
+    </AuthLayout>
   );
 };
 
