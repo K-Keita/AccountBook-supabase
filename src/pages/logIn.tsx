4fe19b042ccfa2052325a7f33b17e104052e9072
kitagawakeita <k.syarin610@gmail.com>
2021-11-13 23:51:09 +0900

    Layoutの不具合調整

--- src/pages/logIn.tsx
+++ src/pages/logIn.tsx
@@ -9,7 +9,6 @@ import { useForm } from "react-hook-form";
 import { AuthButton } from "src/components/utils/authButton";
 import { GoogleButton } from "src/components/utils/googleButton";
 import { Title } from "src/components/utils/title";
-import { AuthLayout } from "src/layouts/authLayout";
 import { client } from "src/libs/supabase";
 
 type FormValues = {
@@ -59,7 +58,10 @@ const LogIn = () => {
   };
 
   return (
-    <AuthLayout>
+    <main
+      className="h-screen text-white bg-gradient-to-b from-blue-800 via-purple-900 to-danger"
+      style={{ fontFamily: "游明朝体" }}
+    >
       <div className="absolute top-0 w-full bg-blue-600 bg-opacity-20">
         <p className="py-3 text-sm text-center cursor-pointer">
           <span className="opacity-80">アカウントをお持ちでない方 </span>
@@ -85,22 +87,6 @@ const LogIn = () => {
         />
         <AuthButton text="ログイン" onClick={handleSubmit(onSubmit)} />
       </form>
-      {/* <input
-        placeholder="メールアドレス"
-        className="block py-2 px-1 my-5 mx-auto w-9/12 bg-white bg-opacity-10"
-      />
-      <input
-        placeholder="パスワード"
-        className="block py-2 px-1 my-5 mx-auto w-9/12 bg-white bg-opacity-10"
-      /> */}
-      {/* <button
-          onClick={() => {
-            signInWithEmail("k.syarin2610@gmail.com", "12345678");
-          }}
-          className="block py-2 my-5 mx-auto w-9/12 text-center text-gray-600 bg-white bg-opacity-80 rounded-sm"
-        >
-          ログイン
-        </button> */}
       <p className="text-xs text-center text-gray-100">
         ログインパスワードを忘れてしまった方は
         <span className="text-blue-500 hover:underline cursor-pointer">
@@ -113,21 +99,10 @@ const LogIn = () => {
         <div className="mx-auto mb-3 w-3/12 border-b" />
       </div>
       <GoogleButton text="Googleでログイン" onClick={signInWithGoogle} />
-      {/* <button
-          onClick={signInWithGoogle}
-          className="flex py-1 px-1 my-6 mx-auto w-9/12 text-center bg-google bg-opacity-50 rounded-sm"
-        >
-          <div className="p-1 h-8 bg-white rounded-sm">
-            <Image
-              alt="Google_icon"
-              src="/icons/Google.png"
-              width={24}
-              height={24}
-            />
-          </div>
-          <p className="flex-1 mt-1 text-center">Googleでログイン</p>
-        </button> */}
-    </AuthLayout>
+      <div className="table absolute bottom-1 mx-auto w-full text-xs text-center">
+        &copy;My-house-account
+      </div>
+    </main>
   );
 };
 
--- src/pages/signUp.tsx
+++ src/pages/signUp.tsx
@@ -8,7 +8,6 @@ import { useForm } from "react-hook-form";
 import { AuthButton } from "src/components/utils/authButton";
 import { GoogleButton } from "src/components/utils/googleButton";
 import { Title } from "src/components/utils/title";
-import { AuthLayout } from "src/layouts/authLayout";
 import { client } from "src/libs/supabase";
 
 type FormValues = {
@@ -63,7 +62,10 @@ const SignUp = () => {
   };
 
   return (
-    <AuthLayout>
+    <main
+      className="h-screen text-white bg-gradient-to-b from-blue-800 via-purple-900 to-danger"
+      style={{ fontFamily: "游明朝体" }}
+    >
       <div className="absolute top-0 w-full bg-blue-600 bg-opacity-20">
         <p className="py-3 text-sm text-center cursor-pointer">
           <span className="opacity-80">既にアカウントをお持ちの方 </span>
@@ -76,22 +78,6 @@ const SignUp = () => {
         <Title />
       </div>
       <GoogleButton text="Googleで登録" onClick={signUpWithGoogle} />
-      {/* <button
-        onClick={() => {
-          signUpWithGoogle();
-        }}
-        className="flex py-1 px-1 mx-auto mt-24 w-9/12 text-center bg-google bg-opacity-50 rounded-sm"
-      >
-        <div className="p-1 h-8 bg-white rounded-sm">
-          <Image
-            alt="Google_icon"
-            src="/icons/Google.png"
-            width={24}
-            height={24}
-          />
-        </div>
-        <p className="flex-1 mt-1 text-center">Googleで登録</p>
-      </button> */}
       <div className="flex mt-8 opacity-80">
         <div className="mx-auto mb-3 w-3/12 border-b" />
         <p>または</p>
@@ -111,15 +97,10 @@ const SignUp = () => {
         />
         <AuthButton text="登録" onClick={handleSubmit(onSubmit)} />
       </form>
-      {/* <button
-        onClick={() => {
-          signUpWithEmail("k.syarin2610@gmail.com", "12345678");
-        }}
-        className="block py-2 my-5 mx-auto w-9/12 text-center text-gray-600 bg-white bg-opacity-80 rounded-sm"
-      >
-        登録
-      </button> */}
-    </AuthLayout>
+      <div className="table absolute bottom-1 mx-auto w-full text-xs text-center">
+        &copy;My-house-account
+      </div>
+    </main>
   );
 };
 
