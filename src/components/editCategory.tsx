f9c7b7270aa955e14d743b0fd3e1bcc0ed625b5a
kitagawakeita <k.syarin610@gmail.com>
2021-11-13 23:49:46 +0900

    引数の不具合発生

--- src/components/editCategory.tsx
+++ src/components/editCategory.tsx
@@ -42,7 +42,7 @@ export const EditCategory = (props: Props) => {
       if (error) {
         alert(error);
       }
-      props.getItemList(props.userData.id.toString(), year, month);
+      props.getItemList(props.userData.userID.toString(), year, month);
       closeModal();
     },
     [props, closeModal]
@@ -109,8 +109,6 @@ export const EditCategory = (props: Props) => {
       }
 
       editItems(newValue, prevValue);
-      props.getItemList(props.userData.id.toString(), year, month);
-      closeModal();
     }
   };
 
@@ -128,21 +126,51 @@ export const EditCategory = (props: Props) => {
       return false;
     }
 
-    props.getItemList(props.userData.id.toString(), year, month);
+
+    props.getItemList(props.userData.userID.toString(), year, month);
     closeModal();
   };
 
   return (
     <>
       <div className="flex justify-around">
-        <PrimaryButton type="button" text="編集" onClick={openModal} />
-        <PrimaryButton
-          type="button"
-          text="削除"
+        <button className="px-2 border-r" onClick={openModal}>
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            className="h-6 w-6 text-blue-400"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke="currentColor"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              strokeWidth={1.3}
+              d="M15.232 5.232l3.536 3.536m-2.036-5.036a2.5 2.5 0 113.536 3.536L6.5 21.036H3v-3.572L16.732 3.732z"
+            />
+          </svg>
+        </button>
+        <button
+          className="px-2"
           onClick={() => {
             removeCategory(props.category);
           }}
-        />
+        >
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            className="h-6 w-6 text-red-400"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke="currentColor"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              strokeWidth={1.3}
+              d="M19 7l-.867 12.142A2 2 0 0116.138 21H7.862a2 2 0 01-1.995-1.858L5 7m5 4v6m4-6v6m1-10V4a1 1 0 00-1-1h-4a1 1 0 00-1 1v3M4 7h16"
+            />
+          </svg>
+        </button>
       </div>
 
       <Transition appear show={isOpen} as={Fragment}>
