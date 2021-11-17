4cd878b4817b958c02e9dde92410668f99487800
kitagawakeita <k.syarin610@gmail.com>
2021-11-17 10:57:04 +0900

    homeアイコンの変更

--- src/components/menuBar.tsx
+++ src/components/menuBar.tsx
@@ -5,6 +5,20 @@ export const MenuBar = () => {
     {
       href: "/",
       icon: (
+        // <svg
+        //   xmlns="http://www.w3.org/2000/svg"
+        //   className="mx-4 w-8 h-8"
+        //   fill="none"
+        //   viewBox="0 0 24 24"
+        //   stroke="currentColor"
+        // >
+        //   <path
+        //     strokeLinecap="round"
+        //     strokeLinejoin="round"
+        //     strokeWidth={1}
+        //     d="M9 12l2 2 4-4M7.835 4.697a3.42 3.42 0 001.946-.806 3.42 3.42 0 014.438 0 3.42 3.42 0 001.946.806 3.42 3.42 0 013.138 3.138 3.42 3.42 0 00.806 1.946 3.42 3.42 0 010 4.438 3.42 3.42 0 00-.806 1.946 3.42 3.42 0 01-3.138 3.138 3.42 3.42 0 00-1.946.806 3.42 3.42 0 01-4.438 0 3.42 3.42 0 00-1.946-.806 3.42 3.42 0 01-3.138-3.138 3.42 3.42 0 00-.806-1.946 3.42 3.42 0 010-4.438 3.42 3.42 0 00.806-1.946 3.42 3.42 0 013.138-3.138z"
+        //   />
+        // </svg>
         <svg
           xmlns="http://www.w3.org/2000/svg"
           className="mx-4 w-8 h-8"
@@ -15,12 +29,12 @@ export const MenuBar = () => {
           <path
             strokeLinecap="round"
             strokeLinejoin="round"
-            strokeWidth={1}
-            d="M9 12l2 2 4-4M7.835 4.697a3.42 3.42 0 001.946-.806 3.42 3.42 0 014.438 0 3.42 3.42 0 001.946.806 3.42 3.42 0 013.138 3.138 3.42 3.42 0 00.806 1.946 3.42 3.42 0 010 4.438 3.42 3.42 0 00-.806 1.946 3.42 3.42 0 01-3.138 3.138 3.42 3.42 0 00-1.946.806 3.42 3.42 0 01-4.438 0 3.42 3.42 0 00-1.946-.806 3.42 3.42 0 01-3.138-3.138 3.42 3.42 0 00-.806-1.946 3.42 3.42 0 010-4.438 3.42 3.42 0 00.806-1.946 3.42 3.42 0 013.138-3.138z"
+            strokeWidth={1.2}
+            d="M3 12l2-2m0 0l7-7 7 7M5 10v10a1 1 0 001 1h3m10-11l2 2m-2-2v10a1 1 0 01-1 1h-3m-6 0a1 1 0 001-1v-4a1 1 0 011-1h2a1 1 0 011 1v4a1 1 0 001 1m-6 0h6"
           />
         </svg>
       ),
-      text: "Register",
+      text: "Home",
     },
     {
       href: "/category",
@@ -45,6 +59,15 @@ export const MenuBar = () => {
     {
       href: "/chart",
       icon: (
+        // <svg
+        //   xmlns="http://www.w3.org/2000/svg"
+        //   className="mx-4 w-8 h-8"
+        //   viewBox="0 0 20 20"
+        //   fill="currentColor"
+        // >
+        //   <path d="M2 10a8 8 0 018-8v8h8a8 8 0 11-16 0z" />
+        //   <path d="M12 2.252A8.014 8.014 0 0117.748 8H12V2.252z" />
+        // </svg>
         <svg
           xmlns="http://www.w3.org/2000/svg"
           className="mx-4 w-8 h-8"
@@ -97,15 +120,16 @@ export const MenuBar = () => {
   ];
 
   return (
-    <div className="flex fixed bottom-0 left-1 z-50 justify-around py-2 w-almostFull bg-gradient-to-r from-blue-900 via-purple-900 to-pink-900 rounded-3xl">
+    <div className="flex fixed bottom-0 left-1 z-50 justify-around py-2 w-full bg-gradient-to-r from-blue-900 via-purple-900 to-pink-900">
       {menu.map((value) => {
-        return(
-        <LinkButton
-          key={value.text}
-          href={value.href}
-          icon={value.icon}
-          text={value.text}
-        />);
+        return (
+          <LinkButton
+            key={value.text}
+            href={value.href}
+            icon={value.icon}
+            text={value.text}
+          />
+        );
       })}
     </div>
   );
