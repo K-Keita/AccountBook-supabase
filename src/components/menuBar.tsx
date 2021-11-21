de100efa65cc9171bc91b9b92ad2e96971db6247
kitagawakeita <k.syarin610@gmail.com>
2021-11-21 17:19:04 +0900

    max-widthの変更

--- src/components/menuBar.tsx
+++ src/components/menuBar.tsx
@@ -1,27 +1,12 @@
 import { LinkButton } from "src/components/utils/linkButton";
 
-export const MenuBar = () => {
   const menu = [
     {
       href: "/",
       icon: (
-        // <svg
-        //   xmlns="http://www.w3.org/2000/svg"
-        //   className="mx-4 w-8 h-8"
-        //   fill="none"
-        //   viewBox="0 0 24 24"
-        //   stroke="currentColor"
-        // >
-        //   <path
-        //     strokeLinecap="round"
-        //     strokeLinejoin="round"
-        //     strokeWidth={1}
-        //     d="M9 12l2 2 4-4M7.835 4.697a3.42 3.42 0 001.946-.806 3.42 3.42 0 014.438 0 3.42 3.42 0 001.946.806 3.42 3.42 0 013.138 3.138 3.42 3.42 0 00.806 1.946 3.42 3.42 0 010 4.438 3.42 3.42 0 00-.806 1.946 3.42 3.42 0 01-3.138 3.138 3.42 3.42 0 00-1.946.806 3.42 3.42 0 01-4.438 0 3.42 3.42 0 00-1.946-.806 3.42 3.42 0 01-3.138-3.138 3.42 3.42 0 00-.806-1.946 3.42 3.42 0 010-4.438 3.42 3.42 0 00.806-1.946 3.42 3.42 0 013.138-3.138z"
-        //   />
-        // </svg>
         <svg
           xmlns="http://www.w3.org/2000/svg"
-          className="mx-4 w-8 h-8"
+          className="mx-4 w-8 h-8 group-hover:text-flower group-hover:animate-wobble-hor-bottom"
           fill="none"
           viewBox="0 0 24 24"
           stroke="currentColor"
@@ -34,6 +19,16 @@ export const MenuBar = () => {
           />
         </svg>
       ),
+      selectIcon: (
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          className="mx-4 w-8 h-8 text-flower"
+          viewBox="0 0 20 20"
+          fill="currentColor"
+        >
+          <path d="M10.707 2.293a1 1 0 00-1.414 0l-7 7a1 1 0 001.414 1.414L4 10.414V17a1 1 0 001 1h2a1 1 0 001-1v-2a1 1 0 011-1h2a1 1 0 011 1v2a1 1 0 001 1h2a1 1 0 001-1v-6.586l.293.293a1 1 0 001.414-1.414l-7-7z" />
+        </svg>
+      ),
       text: "Home",
     },
     {
@@ -41,7 +36,7 @@ export const MenuBar = () => {
       icon: (
         <svg
           xmlns="http://www.w3.org/2000/svg"
-          className="mx-4 w-8 h-8"
+          className="mx-4 w-8 h-8 group-hover:text-flower group-hover:animate-rotate-90-ccw"
           fill="none"
           viewBox="0 0 24 24"
           stroke="currentColor"
@@ -54,23 +49,28 @@ export const MenuBar = () => {
           />
         </svg>
       ),
+      selectIcon: (
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          className="mx-4 w-8 h-8 text-flower"
+          viewBox="0 0 20 20"
+          fill="currentColor"
+        >
+          <path
+            fillRule="evenodd"
+            d="M4 2a2 2 0 00-2 2v11a3 3 0 106 0V4a2 2 0 00-2-2H4zm1 14a1 1 0 100-2 1 1 0 000 2zm5-1.757l4.9-4.9a2 2 0 000-2.828L13.485 5.1a2 2 0 00-2.828 0L10 5.757v8.486zM16 18H9.071l6-6H16a2 2 0 012 2v2a2 2 0 01-2 2z"
+            clipRule="evenodd"
+          />
+        </svg>
+      ),
       text: "Category",
     },
     {
       href: "/chart",
       icon: (
-        // <svg
-        //   xmlns="http://www.w3.org/2000/svg"
-        //   className="mx-4 w-8 h-8"
-        //   viewBox="0 0 20 20"
-        //   fill="currentColor"
-        // >
-        //   <path d="M2 10a8 8 0 018-8v8h8a8 8 0 11-16 0z" />
-        //   <path d="M12 2.252A8.014 8.014 0 0117.748 8H12V2.252z" />
-        // </svg>
         <svg
           xmlns="http://www.w3.org/2000/svg"
-          className="mx-4 w-8 h-8"
+          className="mx-4 w-8 h-8 group-hover:text-flower group-hover:animate-flip-vertical-right"
           fill="none"
           viewBox="0 0 24 24"
           stroke="currentColor"
@@ -89,6 +89,17 @@ export const MenuBar = () => {
           />
         </svg>
       ),
+      selectIcon: (
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          className="mx-4 w-8 h-8 text-flower"
+          viewBox="0 0 20 20"
+          fill="currentColor"
+        >
+          <path d="M2 10a8 8 0 018-8v8h8a8 8 0 11-16 0z" />
+          <path d="M12 2.252A8.014 8.014 0 0117.748 8H12V2.252z" />
+        </svg>
+      ),
       text: "Chart",
     },
     {
@@ -96,7 +107,7 @@ export const MenuBar = () => {
       icon: (
         <svg
           xmlns="http://www.w3.org/2000/svg"
-          className="mx-4 w-8 h-8"
+          className="mx-4 w-8 h-8 group-hover:text-flower group-hover:animate-rotate-center"
           fill="none"
           viewBox="0 0 24 24"
           stroke="currentColor"
@@ -115,18 +126,40 @@ export const MenuBar = () => {
           />
         </svg>
       ),
+      selectIcon: (
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          className="mx-4 w-8 h-8 text-flower"
+          viewBox="0 0 20 20"
+          fill="currentColor"
+        >
+          <path
+            fillRule="evenodd"
+            d="M11.49 3.17c-.38-1.56-2.6-1.56-2.98 0a1.532 1.532 0 01-2.286.948c-1.372-.836-2.942.734-2.106 2.106.54.886.061 2.042-.947 2.287-1.561.379-1.561 2.6 0 2.978a1.532 1.532 0 01.947 2.287c-.836 1.372.734 2.942 2.106 2.106a1.532 1.532 0 012.287.947c.379 1.561 2.6 1.561 2.978 0a1.533 1.533 0 012.287-.947c1.372.836 2.942-.734 2.106-2.106a1.533 1.533 0 01.947-2.287c1.561-.379 1.561-2.6 0-2.978a1.532 1.532 0 01-.947-2.287c.836-1.372-.734-2.942-2.106-2.106a1.532 1.532 0 01-2.287-.947zM10 13a3 3 0 100-6 3 3 0 000 6z"
+            clipRule="evenodd"
+          />
+        </svg>
+      ),
       text: "Setting",
     },
   ];
 
+  type Props = {
+    page: string;
+  }
+
+
+export const MenuBar = (props: Props) => {
+
+
   return (
-    <div className="flex fixed bottom-0 left-1 z-50 justify-around py-2 w-full bg-gradient-to-r from-blue-900 via-purple-900 to-pink-900">
+    <div className="flex fixed bottom-0 left-0 z-50 justify-around py-1 w-full bg-gradient-to-r from-blue-900 via-purple-900 to-pink-800">
       {menu.map((value) => {
         return (
           <LinkButton
             key={value.text}
             href={value.href}
-            icon={value.icon}
+            icon={props.page === value.href ? value.selectIcon : value.icon}
             text={value.text}
           />
         );
