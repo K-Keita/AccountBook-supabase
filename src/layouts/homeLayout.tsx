256231ffee7f88cd7e5923c3b5e21d677e503284
kitagawakeita <k.syarin610@gmail.com>
2021-11-22 15:54:19 +0900

    pc画面headerの追加

--- src/layouts/homeLayout.tsx
+++ src/layouts/homeLayout.tsx
@@ -7,9 +7,6 @@ export const HomeLayout = (page: ReactNode) => {
 
   return (
     <div>
-      <div className="hidden sm:block">
-        <Header />
-      </div>
       <div
         className="text-white bg-gradient-to-b from-blue-800 via-purple-900 to-danger"
         style={{ fontFamily: "游明朝体" }}
