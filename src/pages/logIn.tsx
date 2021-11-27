f2250173d346757402bcb7913095e1ffdfbde063
kitagawakeita <k.syarin610@gmail.com>
2021-11-27 11:05:57 +0900

    最大幅修正

--- src/pages/logIn.tsx
+++ src/pages/logIn.tsx
@@ -62,45 +62,47 @@ const LogIn = () => {
       className="h-screen text-white bg-gradient-to-b from-blue-800 via-purple-900 to-danger"
       style={{ fontFamily: "游明朝体" }}
     >
-      <div className="absolute top-0 w-full bg-blue-600 bg-opacity-20">
-        <p className="py-3 text-sm text-center cursor-pointer">
-          <span className="opacity-80">アカウントをお持ちでない方 </span>
-          <Link href="/signUp" passHref>
-            <span className="hover:underline">登録はこちら</span>
-          </Link>
+      <div className="relative mx-auto max-w-4xl h-screen">
+        <div className="absolute top-0 w-full bg-blue-600 bg-opacity-20">
+          <p className="py-3 text-sm text-center cursor-pointer">
+            <span className="opacity-80">アカウントをお持ちでない方 </span>
+            <Link href="/signUp" passHref>
+              <span className="hover:underline">登録はこちら</span>
+            </Link>
+          </p>
+        </div>
+        <div className="px-8 pt-28 pb-5">
+          <Title />
+        </div>
+        <form onSubmit={handleSubmit(onSubmit)}>
+          <input
+            autoFocus
+            placeholder="メールアドレス"
+            {...register("email")}
+            className="block py-2 px-1 my-5 mx-auto w-9/12 bg-white bg-opacity-10"
+          />
+          <input
+            placeholder="パスワード"
+            {...register("password")}
+            className="block py-2 px-1 my-5 mx-auto w-9/12 bg-white bg-opacity-10"
+          />
+          <AuthButton text="ログイン" onClick={handleSubmit(onSubmit)} />
+        </form>
+        <p className="text-xs text-center text-gray-100">
+          ログインパスワードを忘れてしまった方は
+          <span className="text-blue-500 hover:underline cursor-pointer">
+            こちら
+          </span>
         </p>
-      </div>
-      <div className="px-8 pt-28 pb-5">
-        <Title />
-      </div>
-      <form onSubmit={handleSubmit(onSubmit)}>
-        <input
-          autoFocus
-          placeholder="メールアドレス"
-          {...register("email")}
-          className="block py-2 px-1 my-5 mx-auto w-9/12 bg-white bg-opacity-10"
-        />
-        <input
-          placeholder="パスワード"
-          {...register("password")}
-          className="block py-2 px-1 my-5 mx-auto w-9/12 bg-white bg-opacity-10"
-        />
-        <AuthButton text="ログイン" onClick={handleSubmit(onSubmit)} />
-      </form>
-      <p className="text-xs text-center text-gray-100">
-        ログインパスワードを忘れてしまった方は
-        <span className="text-blue-500 hover:underline cursor-pointer">
-          こちら
-        </span>
-      </p>
-      <div className="flex mt-16 opacity-80">
-        <div className="mx-auto mb-3 w-3/12 border-b" />
-        <p className="">または</p>
-        <div className="mx-auto mb-3 w-3/12 border-b" />
-      </div>
-      <GoogleButton text="Googleでログイン" onClick={signInWithGoogle} />
-      <div className="table absolute bottom-1 mx-auto w-full text-xs text-center">
-        &copy;My-house-account
+        <div className="flex mt-16 opacity-80">
+          <div className="mx-auto mb-3 w-3/12 border-b" />
+          <p className="">または</p>
+          <div className="mx-auto mb-3 w-3/12 border-b" />
+        </div>
+        <GoogleButton text="Googleでログイン" onClick={signInWithGoogle} />
+        <div className="table absolute bottom-1 mx-auto w-full text-xs text-center">
+          &copy;My-house-account
+        </div>
       </div>
     </main>
   );
