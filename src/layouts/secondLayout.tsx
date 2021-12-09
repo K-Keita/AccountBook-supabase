e9f6b3b060b8500a425599a4b9f0bc791df6b67b
kitagawakeita <k.syarin610@gmail.com>
2021-12-09 13:58:00 +0900

    不要なレイアウトの削除

--- src/layouts/secondLayout.tsx
+++ src/layouts/secondLayout.tsx
@@ -1,193 +1,24 @@
-import { Title } from "src/components/utils/title";
 import Link from "next/link";
 import { useRouter } from "next/router";
 import type { ReactNode } from "react";
-import { ItemForm } from "src/components/ItemForm";
 import { MenuBar } from "src/components/menuBar";
-import { LinkBar } from "src/components/utils/LinkBar";
-
-const menu = [
-  {
-    href: "/",
-    icon: (
-      <svg
-        xmlns="http://www.w3.org/2000/svg"
-        className="mx-4 w-8 h-8 group-hover:text-flower group-hover:animate-wobble-hor-bottom"
-        fill="none"
-        viewBox="0 0 24 24"
-        stroke="currentColor"
-      >
-        <path
-          strokeLinecap="round"
-          strokeLinejoin="round"
-          strokeWidth={1.2}
-          d="M3 12l2-2m0 0l7-7 7 7M5 10v10a1 1 0 001 1h3m10-11l2 2m-2-2v10a1 1 0 01-1 1h-3m-6 0a1 1 0 001-1v-4a1 1 0 011-1h2a1 1 0 011 1v4a1 1 0 001 1m-6 0h6"
-        />
-      </svg>
-    ),
-    selectIcon: (
-      <svg
-        xmlns="http://www.w3.org/2000/svg"
-        className="mx-4 w-8 h-8 text-flower"
-        viewBox="0 0 20 20"
-        fill="currentColor"
-      >
-        <path d="M10.707 2.293a1 1 0 00-1.414 0l-7 7a1 1 0 001.414 1.414L4 10.414V17a1 1 0 001 1h2a1 1 0 001-1v-2a1 1 0 011-1h2a1 1 0 011 1v2a1 1 0 001 1h2a1 1 0 001-1v-6.586l.293.293a1 1 0 001.414-1.414l-7-7z" />
-      </svg>
-    ),
-    text: "Home",
-  },
-  {
-    href: "/category",
-    icon: (
-      <svg
-        xmlns="http://www.w3.org/2000/svg"
-        className="mx-4 w-8 h-8 group-hover:text-flower group-hover:animate-rotate-90-ccw"
-        fill="none"
-        viewBox="0 0 24 24"
-        stroke="currentColor"
-      >
-        <path
-          strokeLinecap="round"
-          strokeLinejoin="round"
-          strokeWidth={1.2}
-          d="M7 21a4 4 0 01-4-4V5a2 2 0 012-2h4a2 2 0 012 2v12a4 4 0 01-4 4zm0 0h12a2 2 0 002-2v-4a2 2 0 00-2-2h-2.343M11 7.343l1.657-1.657a2 2 0 012.828 0l2.829 2.829a2 2 0 010 2.828l-8.486 8.485M7 17h.01"
-        />
-      </svg>
-    ),
-    selectIcon: (
-      <svg
-        xmlns="http://www.w3.org/2000/svg"
-        className="mx-4 w-8 h-8 text-flower"
-        viewBox="0 0 20 20"
-        fill="currentColor"
-      >
-        <path
-          fillRule="evenodd"
-          d="M4 2a2 2 0 00-2 2v11a3 3 0 106 0V4a2 2 0 00-2-2H4zm1 14a1 1 0 100-2 1 1 0 000 2zm5-1.757l4.9-4.9a2 2 0 000-2.828L13.485 5.1a2 2 0 00-2.828 0L10 5.757v8.486zM16 18H9.071l6-6H16a2 2 0 012 2v2a2 2 0 01-2 2z"
-          clipRule="evenodd"
-        />
-      </svg>
-    ),
-    text: "Category",
-  },
-  {
-    href: "/chart",
-    icon: (
-      <svg
-        xmlns="http://www.w3.org/2000/svg"
-        className="mx-4 w-8 h-8 group-hover:text-flower group-hover:animate-flip-vertical-right"
-        fill="none"
-        viewBox="0 0 24 24"
-        stroke="currentColor"
-      >
-        <path
-          strokeLinecap="round"
-          strokeLinejoin="round"
-          strokeWidth={1.2}
-          d="M11 3.055A9.001 9.001 0 1020.945 13H11V3.055z"
-        />
-        <path
-          strokeLinecap="round"
-          strokeLinejoin="round"
-          strokeWidth={1.2}
-          d="M20.488 9H15V3.512A9.025 9.025 0 0120.488 9z"
-        />
-      </svg>
-    ),
-    selectIcon: (
-      <svg
-        xmlns="http://www.w3.org/2000/svg"
-        className="mx-4 w-8 h-8 text-flower"
-        viewBox="0 0 20 20"
-        fill="currentColor"
-      >
-        <path d="M2 10a8 8 0 018-8v8h8a8 8 0 11-16 0z" />
-        <path d="M12 2.252A8.014 8.014 0 0117.748 8H12V2.252z" />
-      </svg>
-    ),
-    text: "Chart",
-  },
-  {
-    href: "/setting",
-    icon: (
-      <svg
-        xmlns="http://www.w3.org/2000/svg"
-        className="mx-4 w-8 h-8 group-hover:text-flower group-hover:animate-rotate-center"
-        fill="none"
-        viewBox="0 0 24 24"
-        stroke="currentColor"
-      >
-        <path
-          strokeLinecap="round"
-          strokeLinejoin="round"
-          strokeWidth={1.2}
-          d="M10.325 4.317c.426-1.756 2.924-1.756 3.35 0a1.724 1.724 0 002.573 1.066c1.543-.94 3.31.826 2.37 2.37a1.724 1.724 0 001.065 2.572c1.756.426 1.756 2.924 0 3.35a1.724 1.724 0 00-1.066 2.573c.94 1.543-.826 3.31-2.37 2.37a1.724 1.724 0 00-2.572 1.065c-.426 1.756-2.924 1.756-3.35 0a1.724 1.724 0 00-2.573-1.066c-1.543.94-3.31-.826-2.37-2.37a1.724 1.724 0 00-1.065-2.572c-1.756-.426-1.756-2.924 0-3.35a1.724 1.724 0 001.066-2.573c-.94-1.543.826-3.31 2.37-2.37.996.608 2.296.07 2.572-1.065z"
-        />
-        <path
-          strokeLinecap="round"
-          strokeLinejoin="round"
-          strokeWidth={1.2}
-          d="M15 12a3 3 0 11-6 0 3 3 0 016 0z"
-        />
-      </svg>
-    ),
-    selectIcon: (
-      <svg
-        xmlns="http://www.w3.org/2000/svg"
-        className="mx-4 w-8 h-8 text-flower"
-        viewBox="0 0 20 20"
-        fill="currentColor"
-      >
-        <path
-          fillRule="evenodd"
-          d="M11.49 3.17c-.38-1.56-2.6-1.56-2.98 0a1.532 1.532 0 01-2.286.948c-1.372-.836-2.942.734-2.106 2.106.54.886.061 2.042-.947 2.287-1.561.379-1.561 2.6 0 2.978a1.532 1.532 0 01.947 2.287c-.836 1.372.734 2.942 2.106 2.106a1.532 1.532 0 012.287.947c.379 1.561 2.6 1.561 2.978 0a1.533 1.533 0 012.287-.947c1.372.836 2.942-.734 2.106-2.106a1.533 1.533 0 01.947-2.287c1.561-.379 1.561-2.6 0-2.978a1.532 1.532 0 01-.947-2.287c.836-1.372-.734-2.942-2.106-2.106a1.532 1.532 0 01-2.287-.947zM10 13a3 3 0 100-6 3 3 0 000 6z"
-          clipRule="evenodd"
-        />
-      </svg>
-    ),
-    text: "Setting",
-  },
-];
-
 
 export const SecondLayout = (page: ReactNode) => {
   const router = useRouter();
 
   return (
-    <>
-      <div
-        className="text-white bg-gray-600"
-        style={{ fontFamily: "游明朝体" }}
-      >
-        <div className="mx-auto bg-blue-800 sm:flex">
-          <div className="w-80 hidden sm:block">
-            <div className="py-3 px-4">
-              <Title />
-            </div>
-            {menu.map((value) => {
-              return (
-                <LinkBar
-                  key={value.text}
-                  href={value.href}
-                  icon={value.icon}
-                  text={value.text}
-                />
-              );
-            })}
-          </div>
-          <main className="overflow-hidden relative z-40 w-full min-h-screen bg-home rounded-t-3xl animate-slide-in-bottom sm:ml-auto sm:max-w-3xl md:p-5">
-            <Link href="/" passHref>
-              <button className="p-6 text-2xl sm:hidden">-Title-</button>
-            </Link>
-            {page}
-          </main>
-          <ItemForm />
-        </div>
-        <div>
-          <MenuBar page={router.pathname} />
-        </div>
+    <div className="text-white bg-gray-600" style={{ fontFamily: "游明朝体" }}>
+      <div className="justify-center mx-auto bg-blue-800 sm:flex">
+        <main className="overflow-hidden relative z-40 w-full min-h-screen bg-home rounded-t-3xl animate-slide-in-bottom sm:rounded-none sm:animate-none md:p-5">
+          <Link href="/" passHref>
+            <button className="p-6 text-2xl sm:hidden">-Title-</button>
+          </Link>
+          {page}
+        </main>
       </div>
-    </>
+      <div className="block sm:hidden">
+        <MenuBar page={router.pathname} />
+      </div>
+    </div>
   );
 };
