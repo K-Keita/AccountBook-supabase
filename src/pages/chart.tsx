deb1920eb1687bdae0ba50ef8d8cd8070f261bb8
kitagawakeita <k.syarin610@gmail.com>
2021-12-09 13:58:37 +0900

    Chartのズレ修正

--- src/pages/chart.tsx
+++ src/pages/chart.tsx
@@ -1,10 +1,16 @@
 import { useEffect } from "react";
 import { Graph } from "src/components/Graph";
+import { LinkButtonList } from "src/components/LinkButtonList";
 import { ChangeMonthButton } from "src/components/utils/changeMonthButton";
+import { PriceDisplay } from "src/components/utils/PriceDisplay";
+import { Title as TitleArea } from "src/components/utils/title";
 import { useChangeMonth } from "src/hooks/useChangeMonth";
 import { useGetItemList } from "src/hooks/useGetItemList";
 import { SecondLayout } from "src/layouts/secondLayout";
 import { client } from "src/libs/supabase";
+
+const d = new Date();
+const date = d.getDate();
 
 const Chart = () => {
   const user = client.auth.user();
@@ -28,9 +34,31 @@ const Chart = () => {
     return totalPrice;
   });
 
+
+    const item = itemList.filter((value) => {
+      return value.buyDate[2] === date.toString();
+    });
+    const totalItemsPrice = item.reduce((sum, element) => {
+      return sum + element.price;
+    }, 0);
+
   return userData ? (
-    <div className="sm:overflow-y-scroll sm:h-screen">
-      <section>
+    <div className="sm:flex sm:overflow-y-scroll">
+      <section className="hidden fixed py-2 w-full h-lg sm:block sm:relative sm:max-w-2xl">
+        <h2 className="px-3 text-2xl">TITLE</h2>
+        <div className="flex flex-col h-3lg">
+          <div className="py-4 px-8">
+            <TitleArea />
+          </div>
+          <PriceDisplay
+            totalPrice={totalPrice}
+            targetAmount={userData.targetAmount}
+            totalItemsPrice={totalItemsPrice}
+          />
+          <LinkButtonList />
+        </div>
+      </section>
+      <section className="sm:w-1/3">
         <div className="flex justify-between my-3">
           <h2 className="px-4 text-4xl font-bold">Chart</h2>
           <div className="px-8 mt-5">
@@ -43,7 +71,7 @@ const Chart = () => {
         </div>
         <Graph arr={priceArr} labels={userData.categoryList} />
       </section>
-      <section >
+      <section className="sm:w-1/3">
         <h2 className="px-4 text-4xl font-bold">By Category</h2>
         <p className="p-4 text-lg text-right">
           合計<span>({itemList.length})</span>:{""}
