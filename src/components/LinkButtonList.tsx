ebc5f6768469e2ae9e38e57834beb2752bdeca6d
kitagawakeita <k.syarin610@gmail.com>
2021-12-13 16:15:58 +0900

    💄 スタイルの変更

--- src/components/LinkButtonList.tsx
+++ src/components/LinkButtonList.tsx
@@ -1,13 +1,91 @@
 import { LinkButton } from "src/components/utils/linkButton";
 
+import { LinkBar } from "./utils/LinkBar";
+
 export const LinkButtonList = () => {
   return (
-    <div className="flex justify-around py-2 my-2">
-      <LinkButton
+    <>
+      <div className="flex justify-around py-2 my-2 sm:hidden">
+        <LinkButton
+          href="/category"
+          icon={
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              className="mx-4 w-8 h-8 group-hover:text-flower group-hover:animate-rotate-90-ccw"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth={1.2}
+                d="M7 21a4 4 0 01-4-4V5a2 2 0 012-2h4a2 2 0 012 2v12a4 4 0 01-4 4zm0 0h12a2 2 0 002-2v-4a2 2 0 00-2-2h-2.343M11 7.343l1.657-1.657a2 2 0 012.828 0l2.829 2.829a2 2 0 010 2.828l-8.486 8.485M7 17h.01"
+              />
+            </svg>
+          }
+          text={"Category"}
+        />
+        <LinkButton
+          href="/chart"
+          icon={
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              className="mx-4 w-8 h-8 group-hover:text-flower group-hover:animate-flip-vertical-right"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth={1.2}
+                d="M11 3.055A9.001 9.001 0 1020.945 13H11V3.055z"
+              />
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth={1.3}
+                d="M20.488 9H15V3.512A9.025 9.025 0 0120.488 9z"
+              />
+            </svg>
+          }
+          text="Chart"
+        />
+        <LinkButton
+          href="/setting"
+          icon={
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              className="mx-4 w-8 h-8 group-hover:text-flower group-hover:animate-rotate-center"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth={1.2}
+                d="M10.325 4.317c.426-1.756 2.924-1.756 3.35 0a1.724 1.724 0 002.573 1.066c1.543-.94 3.31.826 2.37 2.37a1.724 1.724 0 001.065 2.572c1.756.426 1.756 2.924 0 3.35a1.724 1.724 0 00-1.066 2.573c.94 1.543-.826 3.31-2.37 2.37a1.724 1.724 0 00-2.572 1.065c-.426 1.756-2.924 1.756-3.35 0a1.724 1.724 0 00-2.573-1.066c-1.543.94-3.31-.826-2.37-2.37a1.724 1.724 0 00-1.065-2.572c-1.756-.426-1.756-2.924 0-3.35a1.724 1.724 0 001.066-2.573c-.94-1.543.826-3.31 2.37-2.37.996.608 2.296.07 2.572-1.065z"
+              />
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth={1.2}
+                d="M15 12a3 3 0 11-6 0 3 3 0 016 0z"
+              />
+            </svg>
+          }
+          text="Setting"
+        />
+      </div>
+      <div className="hidden sm:block">
+
+      <LinkBar
         href="/category"
         icon={
           <svg
-            xmlns="http://www.w3.org/2000/svg"
+          xmlns="http://www.w3.org/2000/svg"
             className="mx-4 w-8 h-8 group-hover:text-flower group-hover:animate-rotate-90-ccw"
             fill="none"
             viewBox="0 0 24 24"
@@ -23,7 +101,7 @@ export const LinkButtonList = () => {
         }
         text={"Category"}
       />
-      <LinkButton
+      <LinkBar
         href="/chart"
         icon={
           <svg
@@ -49,21 +127,9 @@ export const LinkButtonList = () => {
         }
         text="Chart"
       />
-      <LinkButton
+      <LinkBar
         href="/setting"
         icon={
-          // <svg
-          //   xmlns="http://www.w3.org/2000/svg"
-          //   className="mx-4 w-8 h-8 group-hover:text-flower group-hover:animate-rotate-center"
-          //   viewBox="0 0 20 20"
-          //   fill="currentColor"
-          // >
-          //   <path
-          //     fillRule="evenodd"
-          //     d="M11.49 3.17c-.38-1.56-2.6-1.56-2.98 0a1.532 1.532 0 01-2.286.948c-1.372-.836-2.942.734-2.106 2.106.54.886.061 2.042-.947 2.287-1.561.379-1.561 2.6 0 2.978a1.532 1.532 0 01.947 2.287c-.836 1.372.734 2.942 2.106 2.106a1.532 1.532 0 012.287.947c.379 1.561 2.6 1.561 2.978 0a1.533 1.533 0 012.287-.947c1.372.836 2.942-.734 2.106-2.106a1.533 1.533 0 01.947-2.287c1.561-.379 1.561-2.6 0-2.978a1.532 1.532 0 01-.947-2.287c.836-1.372-.734-2.942-2.106-2.106a1.532 1.532 0 01-2.287-.947zM10 13a3 3 0 100-6 3 3 0 000 6z"
-          //     clipRule="evenodd"
-          //   />
-          // </svg>
           <svg
             xmlns="http://www.w3.org/2000/svg"
             className="mx-4 w-8 h-8 group-hover:text-flower group-hover:animate-rotate-center"
@@ -82,11 +148,12 @@ export const LinkButtonList = () => {
               strokeLinejoin="round"
               strokeWidth={1.2}
               d="M15 12a3 3 0 11-6 0 3 3 0 016 0z"
-            />
+              />
           </svg>
         }
         text="Setting"
-      />
-    </div>
+        />
+        </div>
+    </>
   );
 };
--- src/layouts/header.tsx
+++ src/layouts/header.tsx
@@ -4,25 +4,27 @@ import { client } from "src/libs/supabase";
 
 export const Header = () => {
   return (
-    <header className="flex fixed top-0 z-50 justify-between px-5 w-full h-6 bg-blue-900">
-      <Link href="/">
-        <a className="text-3xl">
-          <h2 className="font-bold text-white shadow">-Title-</h2>
-        </a>
-      </Link>
-      <div className="text-blue-600">
-        <Button
-          size="tiny"
-          type="text"
-          className="text-blue-600"
-          icon={<IconLogOut />}
-          onClick={() => {
-            return client.auth.signOut();
-          }}
-        >
-          Sign Out
-        </Button>
-      </div>
-    </header>
+    <>
+      <header className="hidden fixed top-0 z-50 justify-between px-5 w-full h-10 bg-blue-900 sm:flex">
+        <Link href="/">
+          <a className="text-3xl">
+            <h2 className="font-bold text-white shadow">-Title-</h2>
+          </a>
+        </Link>
+        <div className="text-blue-600">
+          <Button
+            size="tiny"
+            type="text"
+            className="text-blue-600"
+            icon={<IconLogOut />}
+            onClick={() => {
+              return client.auth.signOut();
+            }}
+          >
+            Sign Out
+          </Button>
+        </div>
+      </header>
+    </>
   );
 };
