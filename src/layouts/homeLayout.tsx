fdb0448a6230b4d0f8b5ad92eddbc1fe4ef07b71
kitagawakeita <k.syarin610@gmail.com>
2021-12-17 17:09:59 +0900

    レイアウトのエリア変更

--- src/layouts/homeLayout.tsx
+++ src/layouts/homeLayout.tsx
@@ -1,12 +1,14 @@
 import type { ReactNode } from "react";
+import { Header } from "src/layouts/header";
 
 export const HomeLayout = (page: ReactNode) => {
   return (
     <div
-      className="text-white sm:bg-gray-600"
+      className="text-white sm:bg-blue-800"
       style={{ fontFamily: "游明朝体" }}
     >
-      <div className="mx-auto h-screen sm:bg-blue-800">{page}</div>
+      <Header />
+      <main className="mx-auto max-w-[1100px] h-screen sm:border-r sm:bg-home sm:border-l">{page}</main>
     </div>
   );
 };
--- src/layouts/secondLayout.tsx
+++ src/layouts/secondLayout.tsx
@@ -1,4 +1,3 @@
-import Link from "next/link";
 import { useRouter } from "next/router";
 import type { ReactNode } from "react";
 import { MenuBar } from "src/components/menuBar";
@@ -10,15 +9,11 @@ export const SecondLayout = (page: ReactNode) => {
   return (
     <div className="text-white bg-gray-600" style={{ fontFamily: "游明朝体" }}>
       <Header />
-      <div className="justify-center mx-auto bg-blue-800 sm:flex">
-        <main className="overflow-hidden relative z-40 w-full min-h-screen bg-home rounded-t-3xl animate-slide-in-bottom sm:h-screen sm:rounded-none sm:animate-none md:p-5">
-          <div className="hidden h-10 sm:block" />
-          <Link href="/" passHref>
-            <button className="p-6 text-2xl sm:hidden">-Title-</button>
-          </Link>
+      <main className="justify-center mx-auto bg-blue-800 sm:flex">
+        <div className="overflow-hidden relative z-40 w-full min-h-screen bg-home rounded-t-3xl animate-slide-in-bottom sm:mx-auto sm:max-w-[1100px] sm:h-screen sm:border-r sm:border-l sm:rounded-none sm:animate-none">
           {page}
-        </main>
-      </div>
+        </div>
+      </main>
       <MenuBar page={router.pathname} />
     </div>
   );
