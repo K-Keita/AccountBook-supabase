a96003f6aad1717b96da5762b2ef06aee2ecff95
kitagawakeita <k.syarin610@gmail.com>
2021-12-17 17:07:17 +0900

    コンテナの幅変更

--- src/components/sectionContainer/chartContainer.tsx
+++ src/components/sectionContainer/chartContainer.tsx
@@ -1,29 +1,19 @@
-import { Graph } from "../Graph";
-import { ChangeMonthButton } from "../utils/changeMonthButton";
-
 type Props = {
-  prevMonth: () => void;
-  nextMonth: () => void;
-  month: number;
-  priceArr: number[] | undefined;
-  categoryList: string[];
+  changeMonthButton: JSX.Element;
+  graph: JSX.Element;
 };
 
 export const ChartContainer = (props: Props) => {
 
   return (
-    <section className="border-white sm:h-screen sm:border-l">
-      <div className="flex justify-between my-3">
+    <section className="border-white sm:pt-16">
+      <div className="flex justify-between my-3 sm:my-0">
         <h2 className="px-4 text-4xl font-bold">Chart</h2>
         <div className="px-8 mt-5 sm:mt-0">
-          <ChangeMonthButton
-            prevMonth={props.prevMonth}
-            nextMonth={props.nextMonth}
-            month={props.month}
-          />
+          {props.changeMonthButton}
         </div>
       </div>
-      <Graph arr={props.priceArr} labels={props.categoryList} />
+      {props.graph}
     </section>
   );
 };
