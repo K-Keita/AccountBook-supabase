ed27fc8a1e645ea68fbc26f11c8c1deaf9877f08
kitagawakeita <k.syarin610@gmail.com>
2021-12-31 00:14:58 +0900

    headerの編集

--- src/layouts/header.tsx
+++ src/layouts/header.tsx
@@ -1,21 +1,23 @@
 import { Button, IconLogOut } from "@supabase/ui";
 import Link from "next/link";
+// import { LinkButtonList } from "src/components/LinkButtonList";
+// import { MenuBar } from "src/components/menuBar";
 import { client } from "src/libs/supabase";
 
 export const Header = () => {
   return (
     <>
-      <header className="hidden fixed top-0 z-50 justify-between px-5 w-full h-10 bg-blue-900 sm:flex">
+      <header className="hidden fixed top-0 z-30 justify-between px-5 py-3 w-full h-16 bg-gradient-to-r from-blue-900 via-purple-900 to-pink-800 sm:flex">
         <Link href="/">
           <a className="text-3xl">
             <h2 className="font-bold text-white shadow">-Title-</h2>
           </a>
         </Link>
-        <div className="text-blue-600">
+        <div className="text-white">
           <Button
             size="tiny"
             type="text"
-            className="text-blue-600"
+            className="bg-white"
             icon={<IconLogOut />}
             onClick={() => {
               return client.auth.signOut();
