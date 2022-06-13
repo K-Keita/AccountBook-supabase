c5c12558197c17b338889abff4dad2be0dda6a79
kitagawakeita <k.syarin610@gmail.com>
2022-06-13 16:41:45 +0900

    不要なタグの削除

--- src/layouts/header.tsx
+++ src/layouts/header.tsx
@@ -3,22 +3,20 @@ import { client } from "src/libs/supabase";
 
 export const Header = () => {
   return (
-    <>
-      <header className="hidden fixed top-0 z-30 justify-between py-3 px-5 w-full h-16 bg-gradient-to-r from-blue-900 via-purple-900 to-pink-800 sm:flex">
-        <Link href="/">
-          <a className="text-3xl">
-            <h2 className="font-bold text-white shadow">-Title-</h2>
-          </a>
-        </Link>
-        <button
-          className="text-white hover:text-blue-500"
-          onClick={() => {
-            return client.auth.signOut();
-          }}
-        >
-          Sign Out
-        </button>
-      </header>
-    </>
+    <header className="hidden fixed top-0 z-30 justify-between py-3 px-5 w-full h-16 bg-gradient-to-r from-blue-900 via-purple-900 to-pink-800 sm:flex">
+      <Link href="/">
+        <a className="text-3xl">
+          <h2 className="font-bold text-white shadow">-Title-</h2>
+        </a>
+      </Link>
+      <button
+        className="text-white hover:text-blue-500"
+        onClick={() => {
+          return client.auth.signOut();
+        }}
+      >
+        Sign Out
+      </button>
+    </header>
   );
 };
